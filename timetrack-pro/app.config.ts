729cb67ea4e50629554f34e904e302e72472a899
tonypierogi <tonypierogi@users.noreply.github.com>
2026-08-18 16:46:56 -0700

    chore: v1.0.6
    Co-Authored-By: WOZCODE <contact@withwoz.com>

--- timetrack-pro/app.config.ts
+++ timetrack-pro/app.config.ts
@@ -3,7 +3,7 @@ import type { ExpoConfig } from 'expo/config';
 const config: ExpoConfig = {
   name: 'TimeTrack Pro',
   slug: 'timetrack-pro',
-  version: '1.0.5',
+  version: '1.0.6',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'timetrackpro',
